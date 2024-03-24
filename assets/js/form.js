d2ccbd7db620702c0bd367bb7f6fd2d489a7263b
Alex LaPierre <aslapierre7@gmail.com>
2024-03-23 22:23:46 -0500

    added code in form.js to ensure all fields filled out

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,3 +3,25 @@ const modeToggleBtn = document.querySelector('.mode-toggle');
 modeToggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
 });
+
+function submitForm () {
+    user = document.forms["blogForm"]["username"].value;
+    title = document.forms["blogForm"]["title"].value;
+    content = document.forms["blogForm"]["content"].value;
+
+    if (user == "") {
+        alert("Username must be filled out.");
+        // return false;
+    } else if (title == "") {
+        alert("Title must be filled out.");
+        // return false;
+    } else if (content == ""); {
+        alert("Content cannot be blank.");
+        return false;
+    // } else {
+    //     return false;
+    }
+};
+
+
+
